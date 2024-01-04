da6ae289ef674b80ab15c539eee750e1b3d3c877
TheWhiteMachine <priveiro@gmail.com>
2024-01-04 15:26:23 -0800

    changing socket url to deploy

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('https://chat-app-azure-delta.vercel.app/')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
